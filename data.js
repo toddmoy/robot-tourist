6c480bdc204776b4e7a6760ff3b637597b49330f
Todd Moy <todd.moy@gmail.com>
2017-12-09 18:27:08 -0700

    Add data file.

---
+++ data.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: "Reddit",
+    url: "http://reddit.com"
+  },
+  {
+    name: "DesignerNews",
+    url: "http://designernews.co"
+  },
+  {
+    name: "HackerNews",
+    url: "http://news.ycombinator.com"
+  }
+];
